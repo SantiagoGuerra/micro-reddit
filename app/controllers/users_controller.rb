9a6b11cae738cc400cbcf9a8985f421be42f4f37
Santiago Guerra <guerrasantiago19@gmail.com>
2019-12-02 16:45:16 -0500

    user form for to create a new user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def index
+    @user = User.all
+
+  end
+
+  def show
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+ 
+    @user.save
+    redirect_to @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :username)
   end
 end
